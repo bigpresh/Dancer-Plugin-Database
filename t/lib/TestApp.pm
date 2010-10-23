d46a2ac3b551d079bd1ebb1a7ed4f8868619e7fb
franck cuny <franck@lumberjaph.net>
2010-10-23 17:14:10 +0200

    in-memory db
    * db is created in memory, nothing on disk (so we can skip creating fake db in
    temp file)
    * move schema creation to testapp

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -3,6 +3,18 @@ package t::lib::TestApp;
 use Dancer;
 use Dancer::Plugin::Database;
 
+get '/prepare_db' => sub {
+
+    my @sql = (
+        q/create table users (id INTEGER, name VARCHAR(64))/,
+        q/insert into users values (1, 'sukria')/,
+        q/insert into users values (2, 'bigpresh')/,
+    );
+
+    database->do($_) for @sql;
+    'ok';
+};
+
 get '/' => sub {
     my $sth = database->prepare('select count(*) from users');
     $sth->execute;
@@ -19,7 +31,7 @@ get '/user/:id' => sub {
 
 del '/user/:id' => sub {
     my $sth = database->prepare('delete from users where id = ?');
-    $sth->execute(params->{id});
+    $sth->execute( params->{id} );
     'ok';
 };
 

22aeb72486a5d3b2b0acdf9a057a84c928d97a8a
David Precious <davidp@preshweb.co.uk>
2011-02-10 00:56:24 +0000

    Add quick_select; re-work documentation.
    Adding quick_select() convenience method.  It takes a table name and hashref of
    WHERE params, and returns:
    
    - in scalar context, the first matching row as a hashref
    - in list context, a list of all matching rows, as hashrefs.
    
    So, it can DWYM in cases like:
    
        my $employee = database->quick_select('employees', { id => $emp_id });
        say "Hello, " .  $employee->{name};
    
        my @products = database->quick_select('products', { category => $cat_id });
        for my $product (@products) {
            say "Why not buy a nice shiny " . $product->{name};
        }

--- lib/Dancer/Plugin/Database/Handle.pm
+++ lib/Dancer/Plugin/Database/Handle.pm
@@ -5,7 +5,7 @@ use Carp;
 use DBI;
 use base qw(DBI::db);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -13,7 +13,7 @@ Dancer::Plugin::Database::Handle
 
 =head1 DESCRIPTION
 
-Subclassed DBI connection handle with added features
+Subclassed DBI connection handle with added convenience features
 
 
 =head1 SYNOPSIS
@@ -23,6 +23,9 @@ Subclassed DBI connection handle with added features
 
   # Updating a record where id = 42:
   database->quick_update($tablename, { id => 42 }, { foo => 'New value' });
+
+  # Fetching a row quickly
+  my $employee = database->quick_select('employees', { id => $emp_id });
 
 
 =head1 Added features
@@ -53,27 +56,6 @@ sub quick_insert {
 Given a table name, a hashref describing a where clause and a hashref of
 changes, update a row.
 
-The second parameter is a hashref of field => 'value', each of which will be
-included in the WHERE clause used, for instance:
-
-  { id => 42 }
-
-Will result in an SQL query which would include:
-
-  WHERE id = 42
-
-When more than one field => value pair is given, they will be ANDed together:
-
-  { foo => 'Bar', bar => 'Baz' }
-
-Will result in:
-
-  WHERE foo = 'Bar' AND bar = 'Baz'
-
-(Actually, parameterised queries will be used, with placeholders, so SQL
-injection attacks will not work, but it's easier to illustrate as though the
-values were interpolated directly.)
-
 =cut
 
 sub quick_update {
@@ -86,8 +68,8 @@ sub quick_update {
 
   database->quick_delete($table, {  id => 42 });
 
-Given a table name and a hashref to describe the rows which should be deleted,
-delete them.
+Given a table name and a hashref to describe the rows which should be deleted
+(the where clause - see below for further details), delete them.
 
 =cut
 
@@ -96,10 +78,27 @@ sub quick_delete {
     return $self->_quick_query('DELETE', $table_name, undef, $where);
 }
 
+
+=item quick_select
+
+  my $row  = database->quick_select($table, { id => 42 });
+  my @rows = database->quick_select($table, { id => 42 });
+
+Given a table name and a hashref of where clauses (see below for explanation),
+returns either the first matching row as a hashref, if called in scalar context,
+or a list of matching rows as hashrefs, if called in list context.
+
+=cut
+
+sub quick_select {
+    my ($self, $table_name, $where) = @_;
+    return $self->_quick_query('SELECT', $table_name, undef, $where);
+}
+
 sub _quick_query {
     my ($self, $type, $table_name, $data, $where) = @_;
     
-    if ($type !~ m{^ (INSERT|UPDATE|DELETE) $}x) {
+    if ($type !~ m{^ (SELECT|INSERT|UPDATE|DELETE) $}x) {
         carp "Unrecognised query type $type!";
         return;
     }
@@ -113,7 +112,7 @@ sub _quick_query {
         carp "Expected a hashref of changes";
         return;
     }
-    if (($type eq 'UPDATE' || $type eq 'DELETE')
+    if (($type =~ m{INSERT|UPDATE|DELETE})
         && (!$where || ref $where ne 'HASH')) {
         carp "Expected a hashref of where conditions";
         return;
@@ -122,6 +121,7 @@ sub _quick_query {
     $table_name = $self->quote_identifier($table_name);
     my @bind_params;
     my $sql = {
+        SELECT => "SELECT * FROM $table_name ",
         INSERT => "INSERT INTO $table_name ",
         UPDATE => "UPDATE $table_name SET ",
         DELETE => "DELETE FROM $table_name ",
@@ -144,10 +144,31 @@ sub _quick_query {
             map { $self->quote_identifier($_) . '=?' } keys %$where;
         push @bind_params, values %$where;
     }
+    
+    # If it's a select query and we're called in scalar context, we'll only
+    # return one row, so add a LIMIT 1
+    if ($type eq 'SELECT' && !wantarray) {
+        $sql .= ' LIMIT 1';
+    }
+
     Dancer::Logger::debug(
         "Executing query $sql with params " . join ',', @bind_params
     );
-    return $self->do($sql, undef, @bind_params);
+
+    # Select queries, in scalar context, return the first matching row; in list
+    # context, they return a list of matching rows.
+    if ($type eq 'SELECT') {
+        if (wantarray) {
+            return $self->selectall_arrayref(
+                $sql, { Slice => {} }, @bind_params
+            );
+        } else {
+            return $self->selectrow_hashref($sql, undef, @bind_params);
+        }
+
+    } else {
+        return $self->do($sql, undef, @bind_params);
+    }
 }
 
 
@@ -157,6 +178,37 @@ All of the convenience methods provided take care to quote table and column
 names using DBI's C<quote_identifier>, and use parameterised queries to avoid
 SQL injection attacks.  See L<http://www.bobby-tables.com/> for why this is
 important, if you're not familiar with it.
+
+
+=head1 WHERE clauses as hashrefs
+
+C<quick_update>, C<quick_delete> and C<quick_select> take a hashref of WHERE
+clauses.  This is a hashref of field => 'value', each of which will be
+included in the WHERE clause used, for instance:
+
+  { id => 42 }
+
+Will result in an SQL query which would include:
+
+  WHERE id = 42
+
+When more than one field => value pair is given, they will be ANDed together:
+
+  { foo => 'Bar', bar => 'Baz' }
+
+Will result in:
+
+  WHERE foo = 'Bar' AND bar = 'Baz'
+
+(Actually, parameterised queries will be used, with placeholders, so SQL
+injection attacks will not work, but it's easier to illustrate as though the
+values were interpolated directly.  Don't worry, they're not.))
+
+TODO: this isn't very flexible; it would be nice to easily use other logic
+combinations, and other comparisons other than a straightforward equality
+comparison.  However, supporting this abstraction without the syntax used
+becoming a real mess can be... awkward.  Accepting a pre-written SQL 'WHERE'
+clause would be one option.  Any thoughts on this would be appreciated!
 
 
 =head1 AUTHOR

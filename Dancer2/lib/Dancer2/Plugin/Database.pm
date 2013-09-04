801346ee349a08b30ac6428bb29c1a6274194b1f
ambs <ambs@cpan.org>
2013-09-04 16:13:51 +0100

    Bump version

--- Dancer2/lib/Dancer2/Plugin/Database.pm
+++ Dancer2/lib/Dancer2/Plugin/Database.pm
@@ -15,7 +15,7 @@ Dancer2::Plugin::Database - easy database connections for Dancer2 applications
 
 =cut
 
-our $VERSION = '2.09';
+our $VERSION = '2.10';
 
 register_hook qw(database_connected
                  database_connection_lost

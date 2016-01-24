6344359949a1e9664ad252eb94791d1b17ad241a
ambs <ambs@cpan.org>
2016-01-24 20:10:53 +0000

    D2 v2.15

--- Dancer2/lib/Dancer2/Plugin/Database.pm
+++ Dancer2/lib/Dancer2/Plugin/Database.pm
@@ -15,7 +15,7 @@ Dancer2::Plugin::Database - easy database connections for Dancer2 applications
 
 =cut
 
-our $VERSION = '2.14';
+our $VERSION = '2.15';
 
 register_hook qw(database_connected
                  database_connection_lost
